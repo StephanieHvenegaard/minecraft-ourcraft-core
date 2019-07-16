1fe9328571d8096fef175984c2e1c64d5c7f3e92
Stephanie Night <s.hvenegaard@outlook.com>
2019-07-16 20:43:14 +0200

    added bullets

---
+++ src/main/java/com/the_nights/ourcraftmod/core/entity/projectile/EntityBuckshot.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.the_nights.ourcraftmod.core.entity.projectile;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+/**
+ *
+ * @author Stephanie
+ */
+public class EntityBuckshot extends AbstractArrowEntity {
+    public EntityBuckshot(World world)
+    {
+        super(EntityType.ARROW, world);
+    }
+
+    @Override
+    protected ItemStack getArrowStack() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
