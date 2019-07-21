8ffe030066ea2cf856369efd102a9c39b93ffcd4
Stephanie Night <s.hvenegaard@outlook.com>
2019-07-21 23:45:01 +0200

    updated again

---
+++ Ourcraft Armory/src/main/java/com/the_nights/ourcraft/core/items/ItemCustomKatana.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.the_nights.ourcraft.core.items;
+
+import com.the_nights.ourcraft.core.items.materials.ToolMaterial;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+
+/**
+ *
+ * @author Stephanie
+ */
+public class ItemCustomKatana extends SwordItem {
+
+    public ItemCustomKatana(IItemTier itemTier,  Item.Properties props) {
+        super(itemTier, 1, -1.6f, props);
+    
+    }
+
+
+    
+    
+}
